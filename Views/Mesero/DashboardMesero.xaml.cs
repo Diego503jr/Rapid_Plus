9de1d8b6ea0123e0fb826c54f36477e1df4acce0
Sarai Villalta <sarai.villalta12@gmail.com>
2024-10-07 22:49:18 -0600

    Avance Panel de Mesero

--- Views/Mesero/DashboardMesero.xaml.cs
+++ Views/Mesero/DashboardMesero.xaml.cs
@@ -19,9 +19,53 @@ namespace Rapid_Plus.Views.Mesero
     /// </summary>
     public partial class DashboardMesero : Window
     {
+
+        //Instancia de las páginas
+        TomarOrden tomarOrden = new TomarOrden();
+        VerOrden verOrden = new VerOrden();
         public DashboardMesero()
         {
             InitializeComponent();
+        }
+
+        private void BtnCerrarVentana_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void BotonMesero_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Convertir el sender a StackPanel
+            Button boton = sender as Button;
+
+            if (boton != null)
+            {
+                // Cambia el fondo del StackPanel cuando el mouse entra (Hex: #64FF64 - verde claro)
+                boton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD63C3C"));
+            }
+        }
+
+        private void BotonMesero_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Convertir el sender a StackPanel
+            Button boton = sender as Button;
+
+            if (boton != null)
+            {
+                // Cambia el fondo del StackPanel cuando el mouse sale (Hex: #C8C8C8 - gris claro)
+                boton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF008592"));
+            }
+        }
+
+        private void btnTomarOrden_Click(object sender, RoutedEventArgs e)
+        {
+            FrMesero.NavigationService.Navigate(tomarOrden);
+        }
+
+        private void btnVerOrden_Click(object sender, RoutedEventArgs e)
+        {
+            FrMesero.NavigationService.Navigate(verOrden);
         }
     }
 }
