c342040f23e77a99cd499478dece938c306e13b9
Die789 <diegocarias789@gmail.com>
2024-10-09 20:15:17 -0600

    Modelo y formulario de usuario

---
+++ Models/UsuarioModelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rapid_Plus.Models
+{
+    class UsuarioModelo
+    {
+        public int UsuarioId { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int RolId { get; set; }
+        public int DUI { get; set; }
+        public int SexoId {get; set;}
+        public string FechaNacimiento { get; set; }
+    }
+}
